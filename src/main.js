74e08b068e26cf1d9108a24ec42f2196dbbaf94a
muchuan <mu.chuan.hung@gmail.com>
2021-12-04 00:30:20 +0800

    feat: environment setting

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+//新增sass全局設定
+import './assets/stylesheets/scss/all.scss'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
