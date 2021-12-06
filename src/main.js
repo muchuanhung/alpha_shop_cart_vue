ab9370a33f41b9f3a1f071e76a81c97366baa3cf
muchuan <mu.chuan.hung@gmail.com>
2021-12-06 17:12:37 +0800

    feat: add sweetalert function

--- src/main.js
+++ src/main.js
@@ -6,10 +6,14 @@ import './assets/stylesheets/scss/main.scss'
 //新增字型樣式
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
+//新增提示樣式
+import VueSweetalert2 from 'vue-sweetalert2';
 
 Vue.config.productionTip = false
+Vue.use(VueSweetalert2); 
 
 new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
